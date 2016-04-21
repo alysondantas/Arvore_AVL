e15e05b6ff3c3c685c23ef5e8360402a3388e37f
alysondantas <alyson.lipe@gmail.com>
2016-04-20 23:10:22 -0300

    main

---
+++ src/arvores/avl/main.java
@@ -0,0 +1,17 @@
+package arvores.avl;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Arvore arv = new Arvore();
+		arv.inserir(8);
+		arv.inserir(10);
+		arv.inserir(4);
+		arv.inserir(2);
+		arv.inserir(6);
+		arv.inserir(3);
+		arv.imprimeEmOrdem();
+	}
+
+}
